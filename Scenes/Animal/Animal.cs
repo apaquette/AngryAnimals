0ead8a5a068972b0ec819947e58e75bac7cf72e4
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-09-07 13:20:53 -0300

    Implement arrow

--- Scenes/Animal/Animal.cs
+++ Scenes/Animal/Animal.cs
@@ -4,11 +4,13 @@ using Godot;
 public partial class Animal : RigidBody2D
 {
 	private readonly Vector2 DRAG_LIM_MIN = new(-60,0), DRAG_LIM_MAX = new(0,60);
-	private const float IMPULSE_MULT = 20.0f;
+	private const float IMPULSE_MULT = 20.0f, IMUPLSE_MAX = 2000.0f;
 	[Export] private Label _label;
+	[Export] private Sprite2D _arrowSprite;
 	[Export] private AudioStreamPlayer2D _stretchSound, _launchSound, _kickSound;
 
 	private bool _isDragging = false, _isDead = false;
+	private float _arrowScaleX = 0.0f;
 	private Vector2 _dragStart = Vector2.Zero, _start = Vector2.Zero, _dragVector = Vector2.Zero;
 
 	public override void _UnhandledInput(InputEvent @event)
@@ -24,6 +26,8 @@ public partial class Animal : RigidBody2D
 		InputEvent += OnInputEvent;
 		SleepingStateChanged += OnSleepingStateChanged;
 		_start = Position;
+		_arrowScaleX = _arrowSprite.Scale.X;
+		_arrowSprite.Hide();
 	}
 
     
@@ -41,6 +45,7 @@ public partial class Animal : RigidBody2D
 			InputEvent -= OnInputEvent;
 			_isDragging = true;
 			_dragStart = GetGlobalMousePosition();
+			_arrowSprite.Show();
 		}
     }
 
@@ -50,6 +55,7 @@ public partial class Animal : RigidBody2D
 		_isDragging = false;
 		Freeze = false;
 		ApplyCentralImpulse(CalculateImpulse());
+		_arrowSprite.Hide();
 		SignalHub.EmitOnAttemptMade();
 	}
 
@@ -60,6 +66,7 @@ public partial class Animal : RigidBody2D
 			_dragVector = GetGlobalMousePosition() - _dragStart;
 			_dragVector = _dragVector.Clamp(DRAG_LIM_MIN, DRAG_LIM_MAX);
 			Position = _start + _dragVector;
+			ScaleArrow();
 		}
 	}
 
@@ -69,6 +76,17 @@ public partial class Animal : RigidBody2D
 		ds += $"Drag: {_isDragging} Drag Start: {_dragStart} Start: {_start}\n";
 		ds += $"DragVec: {_dragVector}";
 		_label.Text = ds;
+	}
+
+	private void ScaleArrow()
+	{
+		float fraction = CalculateImpulse().Length() / IMUPLSE_MAX;
+		fraction = Mathf.Clamp(fraction, 0.0f, 1.0f);
+		_arrowSprite.Scale = new Vector2(
+			Mathf.Lerp(_arrowScaleX, _arrowScaleX * 2.0f, fraction),
+			_arrowSprite.Scale.Y
+		);
+		_arrowSprite.Rotation = (_start - Position).Angle();
 	}
 
 	private Vector2 CalculateImpulse() => _dragVector * -IMPULSE_MULT;
